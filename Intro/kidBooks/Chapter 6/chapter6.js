8f40e92c7725ba21ed713f724cce688d6ebb3a02
SussyBakaHere <rishensurjoobhalee@gmail.com>
2024-07-30 19:17:17 +0200

    Portfolio Reuploaded

---
+++ Intro/kidBooks/Chapter 6/chapter6.js
@@ -0,0 +1,149 @@
+// let fname = prompt(`What is your name?`);
+// alert(`Hello  ${fname}`);
+
+// if (fname.length > 7) {
+// alert(`Wow, you have a REALLY long name!`);
+// }else {
+//     alert("Your name isn't very long");
+// }
+
+// let lemonChicken = confirm("is there lemon chicken");
+// let beefWithBlackBean = confirm("is there beef with black bean?");
+// let sweetAndSourPork = confirm("is there sweet and sour pork?");
+
+// if(lemonChicken) {
+//     alert("Greet! I am having lemon chicken");
+
+// }else if(beefWithBlackBean) {
+//     alert("Ok I'll take that ");
+
+// }else if(sweetAndSourPork){
+//     alert("well. I'll guess I'll have porkkkk");
+// }else {
+//     alert("rice then");
+// }
+// alert("after if statement : Eating now");
+
+// let myName = prompt("What is your name?");
+// if (myName === "Rishen") { 
+//     alert("Hello Me!");
+// }else if (myName === "Dad" ){
+//     alert("Hello Dad");
+// }else if (myName === "Mom"){
+//     alert("Hello Mom");
+// } else {
+//     alert("Hello Stranger!");
+// }
+
+//LOOPS
+
+let sheepCount = 0;
+while (sheepCount < 10) {
+    console.log("I have counted " + sheepCount + " sheep!");
+    sheepCount++;
+}
+// console.log("ZZZZZZz")
+
+// for(let sheepCount = 0;sheepCount < 10; ++sheepCount){
+//     console.log("I have counted " + sheepCount + " sheep!");
+// }
+
+// for (let countDown = 10; countDown > 0; --countDown) {
+//     console.log(`Bomb explodes in ${countDown} !`);
+// }
+// console.log("Area detonated");
+
+// let countDown = 10;
+// while(countDown > 0) {
+//     console.log(`Jump in ${countDown}!`);
+//     --countDown;
+// }
+// console.log("AAAAAHHHHHHH");
+// console.log("WWWWWWWWWEEEEEEE");
+
+// let timesToSayHello = 69;
+// for (let i = 0; i < timesToSayHello; i++) {
+//     console.log("Hello!");
+// }
+
+// let animals = ["Lion", "Flamingo", "Polor Bear", "Boa Constrictor"];
+// for(let i = 0; i < animals.length; i++) {
+//     console.log(`This zoo contains a ${animals[i]}.`);
+// }
+
+// let myName = "Rishen";
+// for( let i = 0; i < myName.length; i++){
+//     console.log(`My name contains the letter ${myName[i]}.`);
+// }
+
+// for(let x = 2; x < 10000; x = x * 2){
+//     console.log(x);
+// }
+
+// let y = 3
+// while(y < 10000) {
+//     console.log(`${y}`);
+//     y *= 3;
+// }
+
+// let pets = ["Dog", "Cat", "Bird", "Snake", "Rabbit"];
+// for(let i = 0; i < pets.length; i++ ) {
+//     pets[i] = "Awesome " + pets[i];
+// }
+// console.log(pets);
+
+// let alphabet = "abcdefghijklmnopqrstuvwxyz";
+// let randomString = "";
+// let i = 0;
+// // while( i < 6) { 
+// //      randomString += alphabet[Math.floor(Math.random() * 26)];
+// //      i++
+// // }
+// // console.log(randomString);
+
+// while(i < 6) {
+//     randomIndex = Math.floor(Math.random() * 26);
+//     randomLetter = alphabet[randomIndex];
+//     randomString += randomLetter;
+//     i++;
+// }
+// console.log(randomString)
+
+// let input = "javascript is awesome";
+// let output = "";
+// for (let i = 0; i < input.length; i++) {
+
+//     if (input[i] === "a") {
+//         output += "4";
+//     } else if (input[i] === "e") {
+//         output += "3";
+
+//     } else if (input[i] === "i") {
+//         output += "1";
+
+//     } else if (input[i] === "o") {
+
+
+//         output += "0";
+//     } else {
+//         output += input[i];
+//     }
+
+// }
+// console.log(output);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
