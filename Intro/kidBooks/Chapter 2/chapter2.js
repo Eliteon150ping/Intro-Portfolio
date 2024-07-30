cefaa8ff3402d482550ad3f7941774a327ed5584
SussyBakaHere <rishensurjoobhalee@gmail.com>
2024-07-30 19:15:36 +0200

    Portfolio Reuploaded

---
+++ Intro/kidBooks/Chapter 2/chapter2.js
@@ -0,0 +1,221 @@
+//console.log(69*69);
+// console.log("This is a slice".slice(0,4));
+// console.log(true && false);
+
+//Numbers and operators
+
+//console.log("Adding" + 12345 + 56789);
+
+//console.log("Adding three numbers" + (22 + 33 + 44));
+
+//console.log("Subtracting" + (1000-17));
+
+//console.log("Multiplication" + 123*456);
+
+//console.log("Division" + (12345/250));
+
+//console.log(1234 + 57 * 3 -31 / 4);
+
+//console.log(8/(1+3));
+
+//console.log((15 +9)* 2)
+
+//Variables -Day 2
+
+//let age;
+//age =12;
+
+//let age = 69;
+//age=1
+
+//console.log("age is "+age);
+
+//let numberOfSiblings = 1+3;
+//let numberOfCandles = 8;
+//console.log(numberOfCandles / numberOfSiblings);
+
+//let secondsInAMinute = 60;
+//let minutesInAHour = 60;
+//let secondsInAHour = secondsInAMinute * minutesInAHour;
+//console.log(secondsInAHour);
+
+//let hoursInADay = 24;
+//let secondsInADay = secondsInAHour * hoursInADay;
+//console.log(secondsInADay);
+
+//let daysInAYear = 365;
+//let secondsInAYear = secondsInADay * daysInAYear;
+//console.log(secondsInAYear);
+
+//let age = 18;
+//let secondsInMyAge = age * secondsInAYear;
+//console.log("I am " + secondsInMyAge + " seconds old! ");
+
+//Incrementing and Decrementing
+
+//console.log("pre increment and decrement ")
+//let highFives = 69;
+//++highFives;
+//console.log(highFives);
+//++highFives;
+//console.log(highFives);
+//--highFives;
+//console.log(highFives);
+
+//let highFives = 1;
+//console.log(highFives++);
+//console.log(highFives++);
+//console.log(highFives--);
+
+//plus, minus, multiply,divide equals toString
+
+// let x = 10;
+//x = x + 5;
+//console.log(x);
+
+//x += 5;
+//console.log(x);
+
+//x *= 4;
+//console.log(x);
+
+//x /= 3;
+//console.log(x);
+
+//x -= 10;
+//console.log(x);
+
+// x %= 2;
+// console.log(x);
+
+//let balloons = 100;
+//balloons *= 2;
+//console.log(balloons);
+
+ //balloons = 100;
+//balloons /=4;
+//console.log(balloons);
+
+//Strings
+
+//let numberNine = 9;
+//let stringNine = "9";
+//console.log(numberNine + numberNine);
+//console.log(stringNine + stringNine);
+//console.log(numberNine + stringNine);
+//console.log("adding " + 9 + 9);
+//console.log("adding " + (9+9));
+
+//let greeting = "Hello ";
+//let myName =" Rishen";
+//alert(greeting + myName);
+//console.log(greeting + myName)
+
+//console.log("loooooollllll".length)
+
+//let j ="java";
+//let s="script";
+//let js= j + s;
+//console.log(js.length);
+//console.log(js);
+
+//let myName = "Rishen"
+//console.log(myName[myName.length-]);
+
+//let codeWord1 = "super";
+//let codeWord2 = "run";
+//let codeWord3 = "repeat";
+//let codeWord4 = "parry";
+//let codeWord5 = "easy";
+//let code = codeWord1[0] + codeWord2[1] + codeWord3[2] + codeWord4[3] + codeWord5[1]; 
+//alert(code);
+//console.log(code);
+
+//let longString = "My long string is very longish";
+//alert(longString.slice(23,30))
+
+//console.log("hello there, how are you".toUpperCase());
+//console.log("HELLO THERE, HOW ARE YOU".toLocaleLowerCase());
+
+//let myString ="Hello THERE, how ARE you doING?" 
+//myString = myString[0].toUpperCase() + myString.slice(1).toLowerCase();
+//console.log(myString);
+
+//let yourName = prompt("What is your name?");
+// let hadShower = confirm(" Are you single " + yourName + "?")
+// let hasBackPack = confirm(" Are you celebrating valentine's Day" + yourName + "?")
+// alert( yourName + " You sure?" );
+// alert( hadShower && hasBackPack);
+
+// let playFortnite = confirm("Do you not play Fortnite?");
+// let needToShower = !playFortnite;
+// alert( yourName + " you need to shower. " + needToShower);
+
+// let isWeekend = confirm(yourName + " is it weeekend");
+// let hadShower = confirm(yourName + " did you have a shower?");
+// let hasApple = confirm(" Do you have an apple?");
+// let hasOrange = confirm(" Do you have an orange?");
+// let goToSchool = !isWeekend && hadShower && (hasApple || hasOrange);
+// alert(yourName + " do you go to school?" + goToSchool);
+
+// alert(yourName + " Welcome to the theme park!")
+// let height = prompt(" What is your height in inches?")
+// let heightRestriction = 60;
+// let allowed = height >= heightRestriction;
+// alert(yourName + " are you allowed on the ride? " + allowed)
+
+// alert( yourName + " Welcome to the Theme park for kids! ");
+// let height = prompt(" What is your height in inches? ");
+// let heightRestriction = 48;
+// let allowed = height <= heightRestriction;
+// alert(yourName + " are you allowed on this kid's ride?" + allowed);
+
+// Equal to
+
+let mySecretNumber = 5;
+// let chicoGuess = 3;
+// alert(mySecretNumber===chicoGuess);
+// let harpoGuess = 7;
+// alert(mySecretNumber===harpoGuess);
+// let grouchGuess = 5;
+// alert(mySecretNumber=== grouchGuess);
+
+let myGuess =  parseInt(prompt("Guess a number?"));
+alert("Did you guess correctly? " + (myGuess === mySecretNumber));
+
+// alert("Hi, welcome to the movies!");
+// let age = parseInt(prompt("What is your age? "));
+// let accompanied = confirm ("Are you accompanied by a parent?");
+// let message = "Are you allowed to watch the movie? " + (age >= 13 || accompanied);
+// message = message.slice(0,36) + message.slice(36).toUpperCase();
+// alert(message);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
