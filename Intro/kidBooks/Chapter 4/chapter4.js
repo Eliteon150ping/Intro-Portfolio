e73973336a1521da57f5ea316acba62e2f02dc3e
SussyBakaHere <rishensurjoobhalee@gmail.com>
2024-07-30 19:16:28 +0200

    Portfolio Reuploaded

---
+++ Intro/kidBooks/Chapter 4/chapter4.js
@@ -0,0 +1,124 @@
+//Objects
+
+// let cat = {
+//     legs: 3,
+//     "Full name": "Bilal",     //Object keys do not have spaces unless in quotes
+//     color: "Tortoiseshell"
+// };
+// console.log(cat.color);
+// console.log(cat["Full name"]);
+// console.log(cat["name"]);
+
+//let dog = { name: "Rambo", age: 1, color: "Black and Brown", bark: "VRooommm!" };
+// console.log(Object.keys(cat));
+// console.log(Object.keys(dog));
+
+// let cat = {};
+//  cat["name"] = "Harmony";           
+// cat.name = "Harmony";
+// console.log(cat);
+// cat.color = "Tortiseshell";
+// console.log(cat);
+// cat.legs = 3;
+// console.log(cat);
+// console.log(dog.isBrown);
+
+// var dinosaurs = [
+//     { name: "Tyrannosaurus Rex", period: "Late Cretaceous" },
+//     { name: "Stegosaurus", period: "Late Jurassic" },
+//     { name: "Plateosaurus", period: "Triassic" }
+// ];
+// console.log(dinosaurs[0]);
+// console.log(dinosaurs[0].name);
+// console.log(dinosaurs[2].name);
+// console.log(dinosaurs[0].period);
+
+// let anna = { name: "Anna", age: 11, luckNumbers: [2,4,8,16]};
+// let dave = { name: "dave", age: 5, luckNumbers: [3,9,40]};
+// let kate = { name: "kate", age: 9, luckNumbers: [1,2,3]};
+
+// let friends = [anna,dave,kate];
+// console.log(friends[1]);
+// console.log(friends[1].name);
+// console.log("The last lucky number of dave is " + friends[1].luckNumbers[2] +".");
+
+// let owedMoney = {};
+// owedMoney["Jimmy"] = 5;
+// owedMoney["Anna"] = 7;
+// console.log(owedMoney.Jimmy);
+// console.log(owedMoney.Anna);
+// console.log(owedMoney.jiren);
+
+// owedMoney.Jimmy =+ 3;
+// console.log(owedMoney.Jimmy);
+// console.log(owedMoney);
+
+// var movies = {
+//     "Finding Nemo": {
+//         releaseDate: 2003,
+//         duration: 100,
+//         actors: ["Albert Brooks", "Ellen DeGeneres", "Alexander Gould"],
+//         format: "DVD"
+//     },
+//     "Star Wars: Episode VI - Return of the Jedi": {
+//         releaseDate: 1983,
+//         duration: 134,
+//         actors: ["Mark Hamill", "Harrison Ford", "Carrie Fisher"],
+//         format: "DVD"
+//     },
+//     "Harry Potter and the Goblet of Fire": {
+//         releaseDate: 2005,
+//         duration: 157,
+//         actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"],
+//         format: "Blu-ray"
+//     }
+// };
+
+//    let findingNemo = movies["Finding Nemo"];
+//    console.log(findingNemo);
+//    console.log("The duration is " +findingNemo.duration);
+//    console.log("The format is " +findingNemo.format);
+//    console.log(findingNemo.releaseDate);
+//    console.log(findingNemo.actors[0]);
+//    console.log(findingNemo.actors[1]);
+//    console.log(findingNemo.actors[2]);
+
+//    let StarWarsEpisodeVIReturnoftheJedi = movies["Star Wars: Episode VI - Return of the Jedi"];
+//    console.log("One of the actor's is " + StarWarsEpisodeVIReturnoftheJedi.actors[2]+ ".");
+
+// let Cars = {
+//     releaseDate: 2006,
+//     duration: 117,
+//     actors: ["Owen Wilsion", "Bonnie Hunt", "Paul Newman"], 
+//     format: "Blue-ray"
+// };
+// console.log(movies)
+// console.log(movies["Cars"] = Cars);
+// console.log(Object.keys(movies));
+
+// let scores = {Rishen: 0,
+//               Raj:    0, 
+//               Fuz:    0
+            
+//             };
+//             scores.Rishen++;
+//             scores.Raj += 70;
+//             scores.Fuz += 82;
+//             console.log(scores);
+
+// let myCrazyObject = {"name": "A ridiculous object", 
+//                      "some array": [7,9, {purpose: "confusion", number:123 },3.3],
+//                      "random animal": "Banana Shark"
+// };
+// console.log(myCrazyObject["some array"][2].number);
+
+
+
+
+
+
+
+
+
+
+
